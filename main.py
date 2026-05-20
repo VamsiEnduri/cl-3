f1a57d45c4f1cb685dfd1a75c668a7ee9c6ed494
vamsiiiiiiii <vamsi10kcoders@gmail.com>
2026-05-20 09:28:17 +0530

    added dashbiard basic features to

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ login,signup = st.tabs(
     ["Login","SignUp"]
 )
 
+if "user" not in st.session_state:
+    st.session_state.user=None
 
 with login:
     st.header("Login")
@@ -26,8 +28,36 @@ with signup:
 
         btn = st.form_submit_button("SignUp")
 
+        if btn:
+            query="insert into user2(name,email,password) values (%s,%s,%s)"
+
+            values=(name,email,password)
+
+            cursor.execute(query,values)
+            conn.commit()
+            st.success("user added successfully..")
+            st.session_state.user=True
+            st.rerun()
+
         cursor.execute("show tables")
         tables=cursor.fetchall()
         st.write(tables)
 
-        
+
+
+def dashboard():
+    st.sidebar.success(f"welcome to Dashboard") 
+    opt=st.sidebar.selectbox("choose :-- ",["Dashboard","uploadFiles","Logout"])
+    if opt == "Dashboard":
+        st.header("Dashboard")
+    elif opt == "uploadFiles":
+        st.header("uploadFiles")
+    elif opt == "Logout":
+        st.session_state.user=None 
+        st.success("user loggedout successfully")
+        st.rerun()       
+
+if st.session_state.user == True :
+    pass 
+else:
+    dashboard()            
