bf3e7571b8fefd7c482ca5890b586e88d70f9e2d
vamsiiiiiiii <vamsi10kcoders@gmail.com>
2026-05-20 09:17:12 +0530

    db_c file created

---
+++ db_c.py
@@ -0,0 +1,43 @@
+import mysql.connector
+
+
+import streamlit as st
+
+conn = mysql.connector.connect(
+    host=st.secrets["host"],
+    port=st.secrets["port"],
+    user=st.secrets["user"],
+    password=st.secrets["password"],
+    database=st.secrets["database"]
+)
+
+cursor = conn.cursor(dictionary=True)
+
+
+
+# USERS TABLE
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users2(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100),
+    email VARCHAR(100) UNIQUE,
+    password VARCHAR(100)
+)
+""")
+
+# FILES TABLE
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS files2(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
+    file_name VARCHAR(255),
+    file_type VARCHAR(100),
+    file_url TEXT,
+    upload_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY(user_id) REFERENCES users(id)
+)
+""")
+
+conn.commit()
+
+print("Tables Created Successfully")
